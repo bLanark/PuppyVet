26442ed8e4577868e534041024025350f913e4d9
Alistair McDonald <alistair.mcdonald@3davatarschool.com>
2013-05-25 00:32:17 +0100

    added whosepack command

--- src/com/inrevo/puppyvet/Main.java
+++ src/com/inrevo/puppyvet/Main.java
@@ -1,6 +1,11 @@
 package com.inrevo.puppyvet;
 
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -58,6 +63,81 @@ public class Main extends JavaPlugin {
 		return true;
 	}
 
+	private boolean processWhosePackCommand(Player player, String[] args)
+	{
+		int radius = 10;
+		final World world = player.getWorld();
+		if (args.length > 0)
+		{
+			try
+			{
+				radius = Integer.parseInt(args[0]);
+			}
+			catch (NumberFormatException e1)
+			{
+				// nothing to see, move along here 
+			}
+		}
+		if (radius > 20)
+			radius = 20;
+		
+		final Location loc = player.getLocation();
+		int squareRadius = radius * radius;
+		Map<String, Integer> ownerData = new HashMap<String, Integer>();
+
+		for (Chunk chunk : world.getLoadedChunks())
+		{
+			for (Entity entity : chunk.getEntities())
+			{
+				if (loc != null)
+				{
+					if (loc.distanceSquared(entity.getLocation()) > squareRadius)
+					{
+						continue;
+					}
+				}
+				if (entity instanceof Wolf)
+				{
+					Wolf wolf = (Wolf)entity;
+					if (wolf.isTamed())
+					{
+						AnimalTamer target = wolf.getOwner();
+						String name = target.getName();
+						Integer count = ownerData.get(name);
+						if (count == null)
+						{
+							ownerData.put(name, 1);							
+						}
+						else 
+						{
+							ownerData.put(name, count + 1);
+						}
+					}
+				}
+			}
+		}
+		if (ownerData.isEmpty())
+		{
+			String summary = String.format("There were no tamed dogs found within %d blocks", radius);
+			player.sendMessage(summary);
+		}
+		else
+		{
+			String resultString = "The following tamed wolves were found: ";
+			Iterator<?> entries = ownerData.entrySet().iterator();
+			while (entries.hasNext()) 
+			{
+			  Entry<?, ?> thisEntry = (Entry<?, ?>) entries.next();
+			  String owner = (String) thisEntry.getKey();
+			  Integer numDogs = (Integer) thisEntry.getValue();
+			  String thisGuysDogDetails = String.format("\n%s has %d dogs", owner, numDogs);
+			  resultString = resultString + thisGuysDogDetails; 
+			}
+			player.sendMessage(resultString);
+		}
+		return true;
+	}
+
 	private boolean processCalmCommand(Player player, String[] args)
 	{
 		int radius = 10;
@@ -74,7 +154,7 @@ public class Main extends JavaPlugin {
 			}
 		}
 		final Location loc = player.getLocation();
-		int realRadius = radius * radius;
+		int squareRadius = radius * radius;
 		int countAll=0;
 		int countCalmed=0;
 
@@ -84,7 +164,7 @@ public class Main extends JavaPlugin {
 			{
 				if (loc != null)
 				{
-					if (loc.distanceSquared(entity.getLocation()) > realRadius)
+					if (loc.distanceSquared(entity.getLocation()) > squareRadius)
 					{
 						continue;
 					}
@@ -135,6 +215,17 @@ public class Main extends JavaPlugin {
 				return processPackHereCommand(player);
 			}			
 		}
+		if(cmd.getName().equalsIgnoreCase("whosepack"))
+		{ 
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This command can only be run by a player.");
+			} 
+			else 
+			{
+				Player player = (Player) sender;
+				return processWhosePackCommand(player, args);
+			}			
+		}		
 		return false; 
 	}
 }
